35f319c14bc272c43c03cf040a3f28b56dac78b7
niteendalavi <47821887+niteendalavi@users.noreply.github.com>
2021-12-08 00:14:02 +0530

    Add files via upload
    Springboot Sample Example

---
+++ src/test/java/com/example/samples/HelloUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
