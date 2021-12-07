6fffb09a0d6889281a336ef606e15254bfa12e9c
niteendalavi <47821887+niteendalavi@users.noreply.github.com>
2021-12-08 02:27:00 +0530

    Servlet Initializer

--- src/main/java/com/example/samples/HelloUserApplication.java
+++ src/main/java/com/example/samples/HelloUserApplication.java
@@ -1,13 +1,19 @@
 package com.example.samples;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(value="com.example.*")
 @SpringBootApplication
-public class HelloUserApplication {
+public class HelloUserApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloUserApplication.class, args);
 	}
-
+	 @Override
+	    protected SpringApplicationBuilder configure(
+	      SpringApplicationBuilder builder) {
+	        return builder.sources(HelloUserApplication.class);
+	    }
 }
